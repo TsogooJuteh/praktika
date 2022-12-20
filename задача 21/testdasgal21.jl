f4de4a8768ebfa94d851d49e5c7e694e52650d06
TsogooJuteh <114180019+TsogooJuteh@users.noreply.github.com>
2022-12-20 23:34:20 +0300

    Update testdasgal21.jl

--- задача 21/testdasgal21.jl
+++ задача 21/testdasgal21.jl
@@ -2,4 +2,5 @@ using HorizonSideRobots
 include("dasgal21.jl")
 r=Robot("21.sit"; animate=true)
 side=readline()
-side=parse(Int64,side)move_next!(r, HorizonSide(side))
+side=parse(Int64,side)
+move_next!(r, HorizonSide(side))
